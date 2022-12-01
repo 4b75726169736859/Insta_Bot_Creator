c984ab26f1c782e346c62df783bba2e6a8f79476
4b75726169736859 <32509744+4b75726169736859@users.noreply.github.com>
2022-12-01 14:40:16 +0100

    Update BotInsta.py

--- BotInsta.py
+++ BotInsta.py
@@ -1,201 +1,182 @@
+###############################################
+#   Python code written by 4b75726169736859
+###############################################
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.proxy import Proxy, ProxyType
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import WebDriverException
-from selenium.common.exceptions import ElementClickInterceptedException
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementClickInterceptedException, WebDriverException
 import time
 from faker import Faker
 from fake_useragent import UserAgent
 from proxylist import ProxyList
+from mailtm import Email
 import colorful as cf
+from random_username.generate import generate_username
 
-emailDisabled = "britted.com"
 numberAccount = input("How many accounts do you want to create ? ")
-compteur = 0
+counter = 0
 intNumberAccount = int(numberAccount)
 tableMail = []
 tableProxy = []
 tableUsername = []
+codeInsta = None
+trashProxy = []
 
-while compteur < intNumberAccount :
-    #Creation faux nom
+
+def listener(message):
+    global codeInsta
+    codeInsta = message['subject'][0:5]
+    print("code = " + codeInsta)
+
+
+while counter < intNumberAccount:
+    PROXY = None
+    firefox_capabilities = None
+
+    # Creation of fake name, username and email address
     fake = Faker()
     fakeName = fake.name()
+    fakeUsername = generate_username(2)[0]
 
-    #Récuperation de la proxy list
+    email = Email()
+    print("\nDomain: " + email.domain)
+    email.register()
+    print("Email Address: " + str(email.address))
+
+    # Retrieving the proxy list
     pl = ProxyList()
-    pl.load_file('http_proxies.txt')
+    pl.load_file('http_proxies_residential.txt')
     pl.random()
-
-    #configuration du proxy
+    # Proxy setup
     firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
     firefox_capabilities['marionette'] = True
-    PROXY = pl.random().address()
 
-    #firefox_capabilities['proxy'] = {
-        #"proxyType": "MANUAL",
-        #"httpProxy": PROXY,
-        #"sslProxy": PROXY
-    #}
+    while True:
+        if PROXY is None or PROXY not in trashProxy:
+            PROXY = pl.random().address()
+            firefox_capabilities['proxy'] = {
+                "proxyType": "MANUAL",
+                "httpProxy": PROXY,
+                "sslProxy": PROXY
+            }
 
-    #Creation faux useragent
-    #useragent = UserAgent()
-    #profile = webdriver.FirefoxProfile()
-    #profile.set_preference("general.useragent.override", useragent.random)
+    # Create fake useragent
+    useragent = UserAgent()
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("general.useragent.override", useragent.random)
 
-    #ouvrir un navigateur
-    #driver = webdriver.Firefox(profile, capabilities=firefox_capabilities, executable_path=r"C:\Users\ZxFic\Documents\geckodriver\geckodriver.exe")
-    #driver = webdriver.Firefox(capabilities=firefox_capabilities, executable_path=r"C:\Users\ZxFic\Documents\geckodriver\geckodriver.exe")
-    driver = webdriver.Firefox(executable_path=r"C:\Users\ZxFic\Documents\geckodriver\geckodriver.exe")
-    
+    # # Open a browser
+    # driver = webdriver.Firefox(profile, capabilities=firefox_capabilities)       # with fake user-agent + a proxy
+    # driver = webdriver.Firefox(capabilities=firefox_capabilities)        # with a proxy
+    driver = webdriver.Firefox()        # No proxy or fake user-agent
+
     driver.maximize_window()
-
-    #Creation mail jettable
-    try :
-        #driver.get("http://httpbin.org/ip")
-        driver.get("https://www.crazymailing.com")
-    except (NoSuchElementException, TimeoutException, WebDriverException) as e :
-        #print('Erreur : '+str(e))
-        if 'dnsNotFound' in str(e) :
+    try:
+        driver.get('https://www.instagram.com/accounts/emailsignup/')
+    except (NoSuchElementException, TimeoutException, WebDriverException) as e:
+        if 'dnsNotFound' in str(e):
             print(cf.red('\n----- Error : DNS not found -----'))
         elif 'netTimeout' in str(e):
             print(cf.red('\n----- Error : Time Out'))
         elif 'connectionFailure' in str(e):
             print(cf.red('\n----- Error : Connection Failure'))
-        else :
+        else:
             print(cf.red('\n----- Error : Other Error -----'))
         driver.quit()
         continue
-
-    time.sleep(4)
-    try :
-        emailJettable = driver.find_element_by_xpath('//*[@id="email_addr"]').text
-    except NoSuchElementException :
-        print(cf.red('\n----- Error : Access denied -----'))
+    time.sleep(2)
+    try:
+        driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]').click()
+    except NoSuchElementException:
+        driver.find_element(By.XPATH, '/html/body/div[4]/div/div/button[1]').click()
+        print(cf.yellow('\nNo Cookie Accept'))
+    try:
+        driver.find_element(By.NAME, "emailOrPhone").send_keys(str(email.address))
+        driver.find_element(By.NAME, "fullName").send_keys(fakeName)
+        driver.find_element(By.NAME, "username").send_keys(fakeUsername)
+        driver.find_element(By.NAME, "password").send_keys("456password123")
+    except NoSuchElementException:
         driver.quit()
         continue
-    fakeUsername = 'xavier'+emailJettable[0:5]
+    time.sleep(2)
+    try:
+        driver.find_element(By.XPATH,
+                            '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div[7]/div/button').click()
+    except (NoSuchElementException, ElementClickInterceptedException) as e:
+        print(cf.red('\n----- Error -----'))
+        driver.quit()
+        continue
+    time.sleep(5)
+    try:
+        driver.find_element(By.XPATH,
+                            "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select").send_keys(
+            "1999")
+    except (NoSuchElementException, ElementClickInterceptedException) as e:
+        print(cf.red('\n----- Error : Too Many request or Bad proxy -----'))
+        if PROXY is not None:
+            trashProxy.append(PROXY)
+        driver.quit()
+        continue
+    time.sleep(2)
 
-    #Verification mail
-    if emailDisabled in emailJettable:
-        print(cf.red("\n----- Error : Bad mail -----"))
-    else:
-        #ouverture d'un nouvelle onglet
-        driver.execute_script("window.open('');") 
-        driver.switch_to.window(driver.window_handles[1])
-        try :
-            driver.get('https://www.instagram.com/accounts/emailsignup/') 
-        except (NoSuchElementException, TimeoutException, WebDriverException) as e :
-            #print('Erreur : '+str(e))
-            if 'dnsNotFound' in str(e) :
-                print(cf.red('\n----- Error : DNS not found -----'))
-            elif 'netTimeout' in str(e):
-                print(cf.red('\n----- Error : Time Out'))
-            elif 'connectionFailure' in str(e):
-                print(cf.red('\n----- Error : Connection Failure'))
-            else :
-                print(cf.red('\n----- Error : Other Error -----'))
-            driver.quit()
-            continue
-        #Gestion onglet Instagram
-        time.sleep(2)
-        try :
-            driver.find_element_by_xpath('/html/body/div[4]/div/div/button[1]').click()
-            
-        except NoSuchElementException :
-            print(cf.yellow('\nNo Cookie Accept'))
-        try :
-            driver.find_element_by_name("emailOrPhone").send_keys(emailJettable)
-            driver.find_element_by_name("fullName").send_keys(fakeName)
-            driver.find_element_by_name("username").send_keys(fakeUsername)
-            driver.find_element_by_name("password").send_keys("456password123")
-        except NoSuchElementException :
-            driver.quit()
-            continue
-        time.sleep(2)
-        try :
-            driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div[1]/div/form/div[7]/div/button').click()
-        except (NoSuchElementException, ElementClickInterceptedException) as e :
-            #print('error '+e)
-            print(cf.red('\n----- Error : Bad proxy -----'))
-            driver.quit()
-            continue
-        time.sleep(5)
-        try :
-            driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select").send_keys("1999")
-        except (NoSuchElementException, ElementClickInterceptedException) as e :
-            #print('error '+e)
-            print(cf.red('\n----- Error : Bad proxy -----'))
-            driver.quit()
-            continue
-        time.sleep(2)
-        try :
-            driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div[1]/div/div[6]/button').click()
-        except NoSuchElementException :
-            driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/div[6]/button').click()
+    driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div/div[6]/button').click()
+    email.start(listener)
+    print("\nWaiting for new emails...")
 
-        #Changer d'onglet
-        driver.switch_to.window(driver.window_handles[0]) 
-        time.sleep(2)
+    while True:
+        time.sleep(1)
+        if codeInsta is not None:
+            codeDeVerification = str(codeInsta)
+            print("code = " + codeDeVerification)
 
-        #Recuperation du code de verification
+            # Added verification code
+            driver.switch_to.window(driver.window_handles[1])
+            driver.find_element(By.XPATH,
+                                '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[1]/input').send_keys(
+                codeDeVerification)
+            time.sleep(2)
+            driver.find_element(By.XPATH,
+                                '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[2]/button').click()
 
-        emailRecu = driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[3]/div/div/div/div[2]/table/tbody/tr[1]/td[1]/div[2]/strong').text
-        codeDeVerification = emailRecu[0:6]
-        
-        badTime = 0
-        while 'Welc' in codeDeVerification :
-            badTime = badTime + 1
-            if badTime == 30:
-                print(cf.red('\n----- Error : Email not received -----'))
-                driver.quit()
-                continue
-            time.sleep(1)
-            emailRecu = driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[3]/div/div/div/div[2]/table/tbody/tr[1]/td[1]/div[2]/strong').text
-            codeDeVerification = emailRecu[0:6]
+            time.sleep(35) # to adjust, this is the time it will take instagram to verify the code and display your account
 
-        #Ajout du code de verification
-        driver.switch_to.window(driver.window_handles[1])
-        driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[1]/input').send_keys(codeDeVerification)
-        time.sleep(2)
-        driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[2]').click()
-        time.sleep(35)
-
-        #Gestion des erreurs
-        try:
-            message = driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[4]/div').text
-            if 'Sorry ! A problem currently prevents us from verifying your code' in message :
-                print(cf.red("\n----- Error : Unable to verify code -----"))
-            elif 'The IP address you are using has been reported' in message :
-                print(cf.red("\n----- Error : Proxy detect -----"))
-            elif 'Unable to create your account.' in message :
-                print(cf.red("\n----- Error : Unable to create account -----"))
-        except NoSuchElementException:
+            # Error management (XPATHs are probably bad)
             try:
-                driver.find_element_by_xpath('/html/body/div[1]/section/main/div[2]/div/div/div[1]/div[1]/h2')
-                print(cf.red('\n----- Error : Request phone number -----'))
-            except NoSuchElementException :
-                print(cf.green('\nAccount '+fakeUsername+' successfully created !'))
-                tableMail.append(emailJettable)
-                tableProxy.append(PROXY)
-                tableUsername.append(fakeUsername)
-                compteur = compteur + 1
-                with open('mailsInsta.txt', 'w') as f:
-                    for line in tableMail:
-                        f.write(line)
-                        f.write('\n')
-                with open('proxyInsta.txt', 'w') as f:
-                    for line in tableProxy:
-                        f.write(line)
-                        f.write('\n')
-                with open('userInsta.txt', 'w') as f:
-                    for line in tableUsername:
-                        f.write(line)
-                        f.write('\n')
-                time.sleep(10)
+                message = driver.find_element(By.XPATH,
+                                              '/html/body/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[4]/div').text
+                if 'Sorry ! A problem currently prevents us from verifying your code' in message:
+                    print(cf.red("\n----- Error : Unable to verify code -----"))
+                elif 'The IP address you are using has been reported' in message:
+                    print(cf.red("\n----- Error : Proxy detect -----"))
+                elif 'Unable to create your account.' in message:
+                    print(cf.red("\n----- Error : Unable to create account -----"))
+            except NoSuchElementException:
+                try:
+                    driver.find_element(By.XPATH, '/html/body/div[1]/section/main/div[2]/div/div/div[1]/div[1]/h2')
+                    print(cf.red('\n----- Error : Request phone number -----'))
+                except NoSuchElementException:
+                    print(cf.green('\nAccount ' + fakeUsername + ' successfully created !'))
+                    tableMail.append(str(email.address))
+                    if PROXY is not None:
+                        tableProxy.append(PROXY)
+                    tableUsername.append(fakeUsername)
+                    counter = counter + 1
+                    with open('mailsInsta.txt', 'w') as f:
+                        for line in tableMail:
+                            f.write(line)
+                            f.write('\n')
+                    if PROXY is not None:
+                        with open('proxyInsta.txt', 'w') as f:
+                            for line in tableProxy:
+                                f.write(line)
+                                f.write('\n')
+                    with open('userInsta.txt', 'w') as f:
+                        for line in tableUsername:
+                            f.write(line)
+                            f.write('\n')
+                    time.sleep(10)
+        # Exit browser
+        driver.quit()
 
-    #Quitter le navigateur
-    driver.quit()
-
-print(cf.green('\n\nYour '+ numberAccount +' account(s) successfully created !'))
+print(cf.green('\n\nYour ' + numberAccount + ' account(s) successfully created !'))
